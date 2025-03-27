7c9264e9c4f9e7d7da8ee68615e3aac59c6ce700
Bogdan Gaevskij <liltracyn@yandex.ru>
2025-03-27 17:03:03 +0300

    feat: add login and register

--- src/pages/login/login.tsx
+++ src/pages/login/login.tsx
@@ -1,17 +1,20 @@
 import { FC, SyntheticEvent, useState } from 'react';
 import { LoginUI } from '@ui-pages';
-
+import { selectErrorText, fetchLoginUser } from '../../slices/burgerSlice';
+import { useSelector, useDispatch } from '../../services/store';
 export const Login: FC = () => {
+  const dispatch = useDispatch();
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
-
+  const errorText = useSelector(selectErrorText);
   const handleSubmit = (e: SyntheticEvent) => {
     e.preventDefault();
+    dispatch(fetchLoginUser({ email, password }));
   };
 
   return (
     <LoginUI
-      errorText=''
+      errorText={errorText}
       email={email}
       setEmail={setEmail}
       password={password}
