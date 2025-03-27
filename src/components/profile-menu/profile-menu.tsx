3522d7229198dfe077ef55fd1df23e3a969fdca2
Bogdan Gaevskij <liltracyn@yandex.ru>
2025-03-27 17:04:20 +0300

    feat: redirect after logout

--- src/components/profile-menu/profile-menu.tsx
+++ src/components/profile-menu/profile-menu.tsx
@@ -1,11 +1,15 @@
 import { FC } from 'react';
-import { useLocation } from 'react-router-dom';
+import { redirect, useLocation } from 'react-router-dom';
 import { ProfileMenuUI } from '@ui';
-
+import { useDispatch } from '../../services/store';
+import { fetchLogout } from '../../slices/burgerSlice';
 export const ProfileMenu: FC = () => {
   const { pathname } = useLocation();
-
-  const handleLogout = () => {};
+  const dispatch = useDispatch();
+  const handleLogout = () => {
+    dispatch(fetchLogout());
+    redirect('/');
+  };
 
   return <ProfileMenuUI handleLogout={handleLogout} pathname={pathname} />;
 };
