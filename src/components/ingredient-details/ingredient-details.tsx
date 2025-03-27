156d92c63781d5e7951f02441875aeb11ffcf972
Bogdan Gaevskij <liltracyn@yandex.ru>
2025-03-27 15:37:01 +0300

    feat: dispaly in modal details about ingredient

--- src/components/ingredient-details/ingredient-details.tsx
+++ src/components/ingredient-details/ingredient-details.tsx
@@ -1,11 +1,15 @@
 import { FC } from 'react';
 import { Preloader } from '../ui/preloader';
 import { IngredientDetailsUI } from '../ui/ingredient-details';
-
+import { useSelector } from '../../services/store';
+import { selectIngredients } from '../../slices/burgerSlice';
+import { useParams } from 'react-router-dom';
 export const IngredientDetails: FC = () => {
-  /** TODO: взять переменную из стора */
-  const ingredientData = null;
-
+  const ingredientsActive = useParams();
+  const ingredients = useSelector(selectIngredients);
+  const ingredientData = ingredients.find(
+    (ingredient) => ingredient._id === ingredientsActive.id
+  );
   if (!ingredientData) {
     return <Preloader />;
   }
