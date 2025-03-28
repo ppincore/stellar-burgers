4c6d41e50efebdf8198c2c3db66041dacd7bba70
Bogdan Gaevskij <liltracyn@yandex.ru>
2025-03-28 15:25:06 +0300

    feat: display profile orders

--- src/pages/profile-orders/profile-orders.tsx
+++ src/pages/profile-orders/profile-orders.tsx
@@ -1,10 +1,20 @@
 import { ProfileOrdersUI } from '@ui-pages';
 import { TOrder } from '@utils-types';
-import { FC } from 'react';
-
+import { FC, useEffect } from 'react';
+import {
+  fetchUserOrders,
+  selectLoading,
+  selectUserOrders
+} from '../../slices/burgerSlice';
+import { useSelector, useDispatch } from '../../services/store';
+import { Preloader } from '@ui';
 export const ProfileOrders: FC = () => {
-  /** TODO: взять переменную из стора */
-  const orders: TOrder[] = [];
+  const dispatch = useDispatch();
+  const orders = useSelector(selectUserOrders)!;
+  console.log(orders);
+  useEffect(() => {
+    dispatch(fetchUserOrders());
+  }, [dispatch]);
 
-  return <ProfileOrdersUI orders={orders} />;
+  return orders! ? <Preloader /> : <ProfileOrdersUI orders={orders} />;
 };
