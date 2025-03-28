e1aaab28c81bf11525f5120a09adf53995c494f7
Bogdan Gaevskij <liltracyn@yandex.ru>
2025-03-28 15:24:36 +0300

    fix:  from .ts to .tsx

---
+++ src/components/protected-route/protected-route.tsx
@@ -0,0 +1,27 @@
+import React, { ReactElement } from 'react';
+import { useSelector } from '../../services/store';
+import { selectIsAuthenticated, selectIsInit } from '../../slices/burgerSlice';
+import { Navigate, useLocation } from 'react-router-dom';
+import { Preloader } from '@ui';
+
+type TProtectedRouteProps = {
+  children: ReactElement;
+  unAuth?: boolean;
+};
+
+const ProtectedRoute: React.FC<TProtectedRouteProps> = ({
+  children,
+  unAuth
+}) => {
+  const path = useLocation().state?.from || '/';
+  const isAuth = useSelector(selectIsAuthenticated);
+  const isInit = useSelector(selectIsInit);
+
+  if (isInit) return <Preloader />;
+  if (!unAuth && !isAuth) return <Navigate replace to='/login' />;
+  if (unAuth && isAuth) return <Navigate replace to={path} />;
+
+  return children;
+};
+
+export default ProtectedRoute;
