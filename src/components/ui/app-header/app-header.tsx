9f8eb7dbd7e0fcc3c199790842abb889713e5d55
Bogdan Gaevskij <liltracyn@yandex.ru>
2025-03-31 16:42:32 +0300

    refactor: use NavLink instead Link component and fix colors for icons for items in nav menu

--- src/components/ui/app-header/app-header.tsx
+++ src/components/ui/app-header/app-header.tsx
@@ -1,5 +1,4 @@
-import React, { FC } from 'react';
-import clsx from 'clsx';
+import { FC } from 'react';
 import styles from './app-header.module.css';
 import { TAppHeaderUIProps } from './type';
 import {
@@ -8,65 +7,53 @@ import {
   Logo,
   ProfileIcon
 } from '@zlden/react-developer-burger-ui-components';
-import { Link, useLocation } from 'react-router-dom';
-
-export const AppHeaderUI: FC<TAppHeaderUIProps> = ({ userName }) => {
-  const location = useLocation();
-
-  const page = (pageName: string): boolean =>
-    pageName === '/'
-      ? location.pathname === pageName
-      : location.pathname.includes(pageName);
-
-  return (
-    <header className={styles.header}>
-      <nav className={`${styles.menu} p-4`}>
-        <div className={styles.menu_part_left}>
-          <>
-            <BurgerIcon type={'primary'} />
-            <Link
-              to='/'
-              className={clsx({
-                [styles.link_active]: page('/'),
-                [styles.link]: !page('/')
-              })}
-            >
-              <p className='text text_type_main-default ml-2 mr-10'>
-                Конструктор
-              </p>
-            </Link>
-          </>
-          <>
-            <ListIcon type={'primary'} />
-            <Link
-              to='/feed'
-              className={clsx({
-                [styles.link_active]: page('feed'),
-                [styles.link]: !page('feed')
-              })}
-            >
-              <p className='text text_type_main-default ml-2'>Лента заказов</p>
-            </Link>
-          </>
-        </div>
-        <div className={styles.logo}>
-          <Logo className='' />
-        </div>
-        <div className={styles.link_position_last}>
-          <ProfileIcon type={'primary'} />
-          <Link
-            to='/profile'
-            className={clsx({
-              [styles.link_active]: page('profile'),
-              [styles.link]: !page('profile')
-            })}
+import { NavLink } from 'react-router-dom';
+import { page } from '../../../utils/utils';
+export const AppHeaderUI: FC<TAppHeaderUIProps> = ({ userName }) => (
+  <header className={styles.header}>
+    <nav className={`${styles.menu} p-4`}>
+      <div className={styles.menu_part_left}>
+        <>
+          <BurgerIcon type={page('/') ? 'primary' : 'secondary'} />
+          <NavLink
+            to='/'
+            className={({ isActive }) =>
+              isActive ? styles.link_active : styles.link
+            }
           >
-            <p className='text text_type_main-default ml-2'>
-              {userName || 'Личный кабинет'}
+            <p className='text text_type_main-default ml-2 mr-10'>
+              Конструктор
             </p>
-          </Link>
-        </div>
-      </nav>
-    </header>
-  );
-};
+          </NavLink>
+        </>
+        <>
+          <ListIcon type={page('/feed') ? 'primary' : 'secondary'} />
+          <NavLink
+            to='/feed'
+            className={({ isActive }) =>
+              isActive ? styles.link_active : styles.link
+            }
+          >
+            <p className='text text_type_main-default ml-2'>Лента заказов</p>
+          </NavLink>
+        </>
+      </div>
+      <div className={styles.logo}>
+        <Logo className='' />
+      </div>
+      <div className={styles.link_position_last}>
+        <ProfileIcon type={page('/profile') ? 'primary' : 'secondary'} />
+        <NavLink
+          to='/profile'
+          className={({ isActive }) =>
+            isActive ? styles.link_active : styles.link
+          }
+        >
+          <p className='text text_type_main-default ml-2'>
+            {userName || 'Личный кабинет'}
+          </p>
+        </NavLink>
+      </div>
+    </nav>
+  </header>
+);
