e96deec0fca7765bc3b557a36f2086a9bb90c4c5
Bogdan Gaevskij <liltracyn@yandex.ru>
2025-03-31 16:38:23 +0300

    feat: add unique id for ingredients in constructor

--- src/components/burger-ingredient/burger-ingredient.tsx
+++ src/components/burger-ingredient/burger-ingredient.tsx
@@ -1,16 +1,17 @@
 import { FC, memo } from 'react';
 import { useLocation } from 'react-router-dom';
-
 import { BurgerIngredientUI } from '@ui';
 import { TBurgerIngredientProps } from './type';
 import { useDispatch } from '../..//services/store';
 import { addIngredient } from '../../slices/burgerSlice';
+import { v4 as uuid } from 'uuid';
+
 export const BurgerIngredient: FC<TBurgerIngredientProps> = memo(
   ({ ingredient, count }) => {
     const location = useLocation();
     const dispatch = useDispatch();
     const handleAdd = () => {
-      dispatch(addIngredient(ingredient));
+      dispatch(addIngredient({ ...ingredient, id: uuid() }));
     };
 
     return (
