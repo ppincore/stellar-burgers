e85033f136014cdca28158571d483c7be893f933
Bogdan Gaevskij <liltracyn@yandex.ru>
2025-03-31 16:27:39 +0300

    minor changes

--- src/pages/profile-orders/profile-orders.tsx
+++ src/pages/profile-orders/profile-orders.tsx
@@ -1,17 +1,16 @@
 import { ProfileOrdersUI } from '@ui-pages';
-import { TOrder } from '@utils-types';
 import { FC, useEffect } from 'react';
 import {
   fetchUserOrders,
   removeUserOrders,
-  selectLoading,
   selectUserOrders
 } from '../../slices/burgerSlice';
 import { useSelector, useDispatch } from '../../services/store';
 import { Preloader } from '@ui';
+
 export const ProfileOrders: FC = () => {
   const dispatch = useDispatch();
-  const orders = useSelector(selectUserOrders)!;
+  const orders = useSelector(selectUserOrders);
   useEffect(() => {
     dispatch(removeUserOrders());
     dispatch(fetchUserOrders());
