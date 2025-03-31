cea44f5c5210ef9c03c34106dea9bbe7df40461e
Bogdan Gaevskij <liltracyn@yandex.ru>
2025-03-31 16:43:26 +0300

    fixed: moved useEffect from app.tsx to component

--- src/pages/feed/feed.tsx
+++ src/pages/feed/feed.tsx
@@ -1,16 +1,24 @@
 import { Preloader } from '@ui';
 import { FeedUI } from '@ui-pages';
 import { TOrder } from '@utils-types';
-import { FC } from 'react';
+import { FC, useEffect } from 'react';
 import { useSelector, useDispatch } from '../../services/store';
 import { selectOrders, fetchFeed } from '../../slices/burgerSlice';
 
 export const Feed: FC = () => {
   const dispatch = useDispatch();
   const orders: TOrder[] = useSelector(selectOrders);
+
+  useEffect(() => {
+    if (!orders.length) {
+      dispatch(fetchFeed());
+    }
+  }, []);
+
   const handleGetFeeds = () => {
     dispatch(fetchFeed());
   };
+
   return !orders.length ? (
     <Preloader />
   ) : (
